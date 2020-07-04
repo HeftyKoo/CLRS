b0d4ed98af9ea89ebd4315d111c616774bc09c52
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-07-04 19:14:25 +0800

    函数改名

--- code/chapter6/buildMaxHeap.js
+++ code/chapter6/buildMaxHeap.js
@@ -1,6 +1,6 @@
 const maxHeapify = require('./maxHeapify')
 
-function maxHeap (arr) {
+function buildMaxHeap (arr) {
   for (let i = Math.floor(arr.length - 1 / 2); i >= 0; i--) {
     maxHeapify(arr, i)
   }
--- code/chapter6/buildMinHeap.js
+++ code/chapter6/buildMinHeap.js
@@ -1,6 +1,6 @@
 const minHeapify = require('./minHeapify')
 
-function minHeap (arr) {
+function buildMinHeap (arr) {
   for (let i = Math.floor(arr.length - 1 / 2); i >= 0; i--) {
     minHeapify(arr, i)
   }
