9249d174c456e2adb63abb9f4d44223333c25367
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-07-04 17:40:20 +0800

    maxHeapify

---
+++ code/chapter6/maxHeapify.js
@@ -0,0 +1,22 @@
+
+const { parent, left, right} = require('./utils')
+function maxHeapify (arr, i) {
+  const l = left(i)
+  const r = right(i)
+
+  let largest = i
+  if (l <= arr.length - 1 && arr[l] > arr[i]) {
+    largest = l
+  }
+
+  if (r <= arr.length - 1 && arr[r] > arr[largest]) {
+    largest = r
+  }
+
+  if (largest !== i) {
+    temp = arr[i]
+    arr[i] = arr[largest]
+    arr[largest] = temp
+    maxHeapify(arr, largest)
+  }
+}
---
+++ code/chapter6/utils.js
@@ -0,0 +1,14 @@
+// 根节点下标
+exports.parent = function parent (i) {
+  return Math.floor((i + 1) / 2) - 1
+}
+
+// 左节点下标
+exports.left = function left (i) {
+  return 2 * (i + 1) -1
+}
+
+// 右节点下标
+exports.right = function right (i) {
+  return 2 * (i + 1) + 1 - 1
+}
