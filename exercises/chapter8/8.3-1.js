e985e287a3eb43cc1bec64d91b8f49b7f9d29748
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-07-09 20:38:24 +0800

    exercises 8.3-1

---
+++ exercises/chapter8/8.3-1.js
@@ -0,0 +1,34 @@
+function stringRadixSort (arr) {
+  const max = 'z'.charCodeAt()
+  const n = arr[0].length
+
+  for (let cur = n - 1; cur >= 0; cur--) {
+    const countingArr = new Array(max + 1)
+
+    for (let i = 0; i <= max; i++) {
+      countingArr[i] = 0
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      const charCode = arr[i][cur].charCodeAt()
+      countingArr[charCode] = countingArr[charCode] + 1
+    }
+
+    for (let i = 1; i <= max; i++) {
+      countingArr[i] = countingArr[i] + countingArr[i - 1]
+    }
+
+    const result = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+      const current = arr[i]
+      const charCode = current[cur].charCodeAt()
+      const index = countingArr[charCode]
+      result[index - 1] = current
+      countingArr[charCode] = countingArr[charCode] - 1
+    }
+    arr = result
+  }
+  return arr
+}
+
+console.log(stringRadixSort(['COW', 'DOG', 'SEA', 'RUG', 'ROW', 'MOB', 'BOX', 'TAB', 'BAR', 'EAR', 'EAR', 'TAR', 'DUG', 'BIG', 'TEA', 'NOW', 'FOX']))
