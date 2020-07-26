e3daf93527a01619ca1e423b01cea5aa3ff59be5
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-07-26 17:00:21 +0800

    左旋

---
+++ code/chapter13/leftRotate.js
@@ -0,0 +1,18 @@
+function leftRotate (tree, x) {
+  const y = x.right
+  x.right = y.left
+
+  if (y.left) {
+    y.left.p = x
+  }
+  y.p = x.p
+  if (!x.p) {
+    tree.root = y
+  } else if (x === x.p.left) {
+    x.p.left = y
+  } else {
+    x.p.right = y
+  }
+  y.left = x
+  x.p = y
+}
